20a371cd17d74c16f8030402a9034b7e97d5e3d1
teodorfurelid <teodor.furelid@gmail.com>
2025-08-30 13:54:36 +0200

    endringer

--- O3.java
+++ O3.java
@@ -1,19 +1,26 @@
 import java.util.Scanner;
-import static java.lang.Math.*;
 
 public class O3 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Skriv eit tall Z+(Posetive naturlige tall): ");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Skriv eit tall n (n>0): ");
 
-        long n = in.nextInt();
-        long faktuelt = (1*2*3*(n-1)*n);
+        int n = sc.nextInt();
+        int k = 2;
 
-
-        System.out.println("Faktuelt tall: " + faktuelt);
-
-
-
-        in.close();
+        for (int j = 0; j < k; j++) {
+            if (n <= 0) {
+                System.out.println("Ugyldig tall");
+            } else {
+                long fakultet = 1;
+                for (int i = 1; i <= n; i++) {
+                    fakultet *= i;
+                }
+                System.out.println(n + "! er: " + fakultet);
+            }
+            System.out.print("Skriv eit tall n (n>0): ");
+            n = sc.nextInt();
+        }
+        sc.close();
     }
 }
